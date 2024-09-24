6f9438cf3893387f236a4d38269fba7a5458ea10
tatomchedlisvhili <tatomchedlishvili3@gmail.com>
2024-09-24 19:32:41 +0400

    GOA

---
+++ level 03/homework.py
@@ -0,0 +1,20 @@
+age = 17
+age = 22
+
+print(age)
+
+
+mom = 48   
+dad = 46 
+sister = 23
+grandmother = 68 
+
+print( mom + dad + sister + grandmother)
+
+
+
+name = "tato"
+
+name = "nika"
+
+print(name)
