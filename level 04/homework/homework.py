4c76e54c93424dc82434951c07d416004daf7826
tatomchedlisvhili <tatomchedlishvili3@gmail.com>
2024-10-08 18:08:16 +0400

    GOA

--- level 04/homework/homework.py
+++ level 04/homework/homework.py
@@ -3,10 +3,10 @@ surname= input("gvari: ")
 
 print(name, surname)
 
-age=int(input("asaki: "))
+input("asaki: ")
 
 
-print(age + 10)
+print( name + surname)
 
 
 num1 = int(input("chaweret nebismieri ricxvi: "))
@@ -18,13 +18,10 @@ num5 = int(input("chaweret nebismieri ricxvi: "))
 print((num1 + num2 + num3 + num4 + num5) / 5)
 
 
-#case sensitive aris didi asoebit dawerili cvladi 
+#case sensitive nishnavs asoebis sidideze sensitiur enas 
 #snake case yvelaze xshirad gamoyenebadi metodia roca ori an met sityviani cvladis sheqmna gvchirdeba
 
 
-name = tato)
-
-print;(name2)
 
 #name = ("tato")
 #print(name)
