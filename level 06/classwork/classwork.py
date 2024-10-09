2909f0da0666c05a44405b6fbd9dfe510077bd12
tatomchedlisvhili <tatomchedlishvili3@gmail.com>
2024-10-09 14:15:42 +0400

    GOA

---
+++ level 06/classwork/classwork.py
@@ -0,0 +1,20 @@
+age = int(input("chaweret tkveni asaki: "))
+
+print(age // 10)
+
+num = input("chaweret tkveni asaki: ")
+
+print  (type(num))
+
+num2= float(num)
+print (num2)
+print (type(num2))
+
+
+name = input("chaweret tkveni saxeli: ")
+number = int(input("chaweret nebismieri ricxvi: "))
+
+print (name * number)
+
+#explicit - ჩვენი მონაცემთა ტიპის შეცვლა ხელით
+#implicit - ავტომატურად მონაცემთა ტიპის შეცვლა 
---
+++ level 06/homework/homework.py
@@ -0,0 +1,42 @@
+
+#1
+
+num = int(input("chaweret tkveni asaki: "))
+
+num2 = num + 10
+
+print  (num2)
+
+num3= float(num2)
+
+print (num3)
+
+print (type(num3))
+
+
+
+#2
+
+surname= input(" chaweret tkveni gvari: ")
+
+print (surname * 100)
+
+
+
+
+
+
+#3
+
+n1= "va"
+n2= "shli"
+
+print(n1 + n2)
+
+n3 = "sa"
+n4 = "xli"
+
+print (n3 + n4)
+
+
+
