3a9edc203547e9f2e9e4935cf849fa05c4930032
tatomchedlisvhili <tatomchedlishvili3@gmail.com>
2024-10-09 20:32:33 +0400

    GOA

--- level 06/homework/homework.py
+++ level 06/homework/homework.py
@@ -28,15 +28,38 @@ print (surname * 100)
 
 #3
 
-n1= "va"
-n2= "shli"
+n1= "saxli"
+n2= " bina"
+n3 = " agaraki"
+n4 = " dacha"
 
-print(n1 + n2)
+print (n1 + n2 + n3 + n4)
 
-n3 = "sa"
-n4 = "xli"
 
-print (n3 + n4)
+#4
+
+# სტრინგს ვიყენებთ იმისთვის რომ გამოვსახოთ ტექსტური ტიპის ინფორმაცია
+# ინტეჯერს ვიყენებთ მთელი რიცხვებისთვის 
+# floats ვიყენებთ წილადი რიცხვებისთვის
+# boolean-ს ვიყენებთ იმისთვის რომ გამოვსახოთ true ან false  
+
+
+
+#5
+
+name = "tato"
+age = 17
+decimal = 10.2
+
+
+print(type(name))
+print(type(age))
+print(type(decimal))
+
+
+
+
+
 
 
 
