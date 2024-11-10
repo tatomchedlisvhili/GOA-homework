0e94c81774a94ef4a116f05da3af487e49a6530d
tatomchedlisvhili <tatomchedlishvili3@gmail.com>
2024-11-10 17:50:30 +0400

    GOA

---
+++ level 10/classwork/main.py
@@ -0,0 +1,36 @@
+for i in range(50,100,2):
+    print (i)
+
+
+q = int(input("sheiyvanet sityva: "))
+
+for 
+
+
+j = 500    
+
+while j < 700 :
+    print (j)
+    j += 1
+
+k = 50 
+
+while k > 0:
+    print (k)
+    k -= 1
+
+
+
+f = 100
+
+m = int(input("sheiyvanet nebismieri : "))
+
+while m < 100:
+    print (m)
+    m += 1
+
+while m > 100:
+    print (m)
+    m -= 1
+
+
