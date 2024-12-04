b7dbbd4c7a43044ea67abfc8e9e54a44a9b206ef
tatomchedlisvhili <tatomchedlishvili3@gmail.com>
2024-12-04 20:16:30 +0400

    GOA

--- level 12/homework/hm.py
+++ level 12/homework/hm.py
@@ -1,11 +1,48 @@
 #1
 
-number = int(input("chaweret nebismieri ricxvi: "))
+cipri = int(input("sheiyvanet sasurveli ricxvi: "))
 
-if number == 0+2:
-    print(even)
+if cipri % 2 == 0:
+    print("even")
+else:
+    print("odd")
 
 
 #2
 for i in range(0,50,2):
     print (i)
+
+
+
+#3
+
+number = [22,5,9,10,2,15,20,11,13,21]
+
+for i in number:
+    if i > 10:
+        print(i)
+    
+
+
+
+#4
+
+
+numbers = [22,5,9,10,2,15,20,11,13,21]
+
+
+print(numbers[0] - numbers[9])
+print(numbers[0] + numbers[9])
+print(numbers[0] * numbers[9])
+
+#5
+
+ricxvi = int(input("sheiyvanet cipri 0 idan 5is chatvlit:"))
+
+name = ["tato", "nika", "giorgi", "salome", "ana"]
+
+
+print(name[ricxvi])
+
+
+
---
+++ level 14/classwork/main.py
@@ -0,0 +1,32 @@
+
+saxelebi = ["tato","nia","luka","giorgi"]
+s = int(input("shieuyvanet cipri"))
+d = int(input("sheiyvanet cipri"))
+
+print(saxelebi[s:d])
+
+
+
+
+ricxvi = int(input("sheiyvanet cipri 0 idan 5is chatvlit:"))
+
+name = ["tato", "nika", "giorgi", "salome", "ana"]
+
+print(name[ricxvi])
+
+
+
+saxeli = input("sheiyvanet tkveni saxeli: ")
+
+saxeli2 = saxeli[0:3]
+saxeli3 = "tato"
+
+print(saxeli2 + saxeli3[2:])
+
+
+gvari = input("sheiyvanet tkveni gvari: ")
+
+g = gvari
+
+print(gvari)
+print(gvari[::-1])
